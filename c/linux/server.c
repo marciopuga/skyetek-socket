e892733ec6498ab1c329b03398276b9f0ff51746
Marcio Puga <marcio@grumpysailor.com>
2015-11-27 21:16:28 +1100

    50 iterations

--- c/linux/server.c
+++ c/linux/server.c
@@ -29,7 +29,7 @@ int main(int argc , char *argv[])
     int numReaders = 0;
     const int delay = 400000;   //wait at least 400ms after closing the interface before re-opening (USB enumeration)
     const int tests = 3;        //number of open/close tests to perform
-    const int iterations = 10;  //number of select tag operations to perform for each test
+    const int iterations = 50;  //number of select tag operations to perform for each test
     int failures = 0;
     int total = 0;
      
