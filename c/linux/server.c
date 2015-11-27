ee3b7a25d130a50228376bcf9e5793d5e8870e02
Marcio Puga <marcio@grumpysailor.com>
2015-11-27 20:10:56 +1100

    server updated

--- c/linux/server.c
+++ c/linux/server.c
@@ -29,7 +29,7 @@ int main(int argc , char *argv[])
     int numReaders = 0;
     const int delay = 400000;   //wait at least 400ms after closing the interface before re-opening (USB enumeration)
     const int tests = 3;        //number of open/close tests to perform
-    const int iterations = 10;  //number of select tag operations to perform for each test
+    const int iterations = 50;  //number of select tag operations to perform for each test
     int failures = 0;
     int total = 0;
      
@@ -66,75 +66,67 @@ int main(int argc , char *argv[])
     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
     {
         puts("Connection accepted");
-
-       for(int m = 0; m < tests; m++)
-       {
-           total ++;
-           printf("\n\nTEST #%d\n", total);
-           if((numDevices = SkyeTek_DiscoverDevices(&devices)) > 0)
-           {
-               //printf("example: devices=%d", numDevices);
-               if((numReaders = SkyeTek_DiscoverReaders(devices,numDevices,&readers)) > 0 )
-               {
-                   //printf("example: readers=%d\n", numReaders);
-                   for(int i = 0; i < numReaders; i++)
-                   {
-                       printf("Reader Found: %s-%s-%s\n", readers[i]->manufacturer, readers[i]->model, readers[i]->firmware);
-                       for(int k = 0; k < iterations; k++)
-                       {
-                           printf("\tIteration = %d\n",k);
-                           status = SkyeTek_GetTags(readers[0], AUTO_DETECT, &tags, &count);
-                           if(status == SKYETEK_SUCCESS)
-                           {
-                               if(count == 0)
-                               {
-                                   printf("\t\tNo tags found\n");
-                               }
-                               else
-                               {
-                                   for(int j = 0; j < count; j++)
-                                   {
-                                       message = tags[j]->friendly;
-                                       // message = "hello";
-                                       write(client_sock , message , strlen(message));
-                                   }
-                               }
-                           }
-                           else
-                           {
-                               printf("ERROR: GetTags failed\n");
-                           }
-                       }
-                       SkyeTek_FreeTags(readers[i],tags,count);
-                   }
-               }
-               else
-               {
-                   failures ++;
-                   printf("failures = %d/%d\n", failures, total);
-                   printf("ERROR: No readers found\n");
-               }       
-           }
-           else
-           {
-               failures ++;
-               printf("failures = %d/%d\n", failures, total);
-               printf("ERROR: No devices found\n");
-           }
-           SkyeTek_FreeDevices(devices,numDevices);
-           SkyeTek_FreeReaders(readers,numReaders);
-           usleep(delay);
-       }
-        
-        // if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
-        // {
-        //     perror("could not create thread");
-        //     return 1;
-        // }
-         
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( sniffer_thread , NULL);
         puts("Handler assigned");
+    }
+
+    for(int m = 0; m < tests; m++)
+    {
+        total ++;
+        printf("\n\nTEST #%d\n", total);
+        if((numDevices = SkyeTek_DiscoverDevices(&devices)) > 0)
+        {
+            //printf("example: devices=%d", numDevices);
+            if((numReaders = SkyeTek_DiscoverReaders(devices,numDevices,&readers)) > 0 )
+            {
+                //printf("example: readers=%d\n", numReaders);
+                for(int i = 0; i < numReaders; i++)
+                {
+                    printf("Reader Found: %s-%s-%s\n", readers[i]->manufacturer, readers[i]->model, readers[i]->firmware);
+                    for(int k = 0; k < iterations; k++)
+                    {
+                        printf("\tIteration = %d\n",k);
+                        status = SkyeTek_GetTags(readers[0], AUTO_DETECT, &tags, &count);
+                        if(status == SKYETEK_SUCCESS)
+                        {
+                            if(count == 0)
+                            {
+                                printf("\t\tNo tags found\n");
+                            }
+                            else
+                            {
+                                for(int j = 0; j < count; j++)
+                                {
+                                    // message = tags[j]->friendly;
+                                     puts(tags[j]->friendly)
+                                    message = "hello";
+                                    write(client_sock , message , strlen(message));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            printf("ERROR: GetTags failed\n");
+                        }
+                    }
+                    SkyeTek_FreeTags(readers[i],tags,count);
+                }
+            }
+            else
+            {
+                failures ++;
+                printf("failures = %d/%d\n", failures, total);
+                printf("ERROR: No readers found\n");
+            }       
+        }
+        else
+        {
+            failures ++;
+            printf("failures = %d/%d\n", failures, total);
+            printf("ERROR: No devices found\n");
+        }
+        SkyeTek_FreeDevices(devices,numDevices);
+        SkyeTek_FreeReaders(readers,numReaders);
+        usleep(delay);
     }
      
     if (client_sock < 0)
