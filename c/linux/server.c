712c65bb2095c0316705d3a6a6fe00db807f243a
Marcio Puga <marcio@grumpysailor.com>
2015-11-30 15:26:28 +1100

    index fixed

--- c/linux/server.c
+++ c/linux/server.c
@@ -97,7 +97,7 @@ int main(int argc , char *argv[])
                     printf("ERROR: GetTags failed\n");
                 }
             }
-            SkyeTek_FreeTags(readers[i],tags,count);
+            SkyeTek_FreeTags(readers[0],tags,count);
         }
     }
 
