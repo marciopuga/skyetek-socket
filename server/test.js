ae789ba9141f5b848a906e66fa733d3ea9bc8cf3
Marcio Puga <marcio@grumpysailor.com>
2016-01-19 22:06:23 +1100

    opening processes and killing them

--- server/test.js
+++ server/test.js
@@ -1,1 +1,42 @@
-Unix shell commands for Node.js 
+var shell = require('shelljs');
+var ps = require('ps-node');
+
+// or more concisely
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { console.log(stdout) }
+
+
+// exec("subl .", puts);
+// exec("ls", puts);
+
+
+// A simple pid lookup
+ps.lookup({
+    command: 'Sublime',
+    psargs: 'ux'
+    }, function(err, resultList ) {
+    if (err) {
+        throw new Error( err );
+    }
+
+    resultList.forEach(function( process ){
+        if( process ){
+            console.log( 'PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments );
+            if(process.command.indexOf('Sublime') > 0) {
+              kill(process.pid);
+            }
+        }
+    });
+});
+
+function kill(pid) {
+  // A simple pid lookup
+  ps.kill( pid, function( err ) {
+      if (err) {
+          throw new Error( err );
+      }
+      else {
+          console.log( 'Process %s has been killed!', pid );
+      }
+  });
+}
